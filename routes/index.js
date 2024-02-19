404e92aee4ba0152cc24c7352699850e4a9295eb
Lucas YALMAN <yalmanlucas@gmail.com>
2024-02-19 16:43:39 +0100

    pdf front/back link update

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+const PDFDocument = require('pdfkit');
+const express = require('express');
+const fs = require('fs');
+
+const router = express.Router();
+router.post('/pdf', (req, res) => {
+    const { title, address, zipcode, city, content, signature } = req.body;
+    console.log('Received Title:', title);
+    console.log('Received Address:', address);
+    console.log('Received Address:', zipcode);
+    console.log('Received Address:', city);
+    console.log('Received Invoice Content:', content);
+    console.log('Received Signature:', signature);
+
+    const doc = new PDFDocument;
+
+    doc.pipe(res);
+
+    doc.fontSize(22).text(title, { align: 'center' });
+    doc.moveDown();
+
+    doc.fontSize(16).text(address);
+    doc.fontSize(16).text(zipcode + " " + city);
+
+    doc.moveDown();
+
+    doc.fontSize(14).text("Madame, Monsieur,");
+    doc.moveDown();
+    doc.text(content);
+    doc.moveDown();
+
+    doc.fontSize(14).text("Signature :", { underline: true, align: 'right' });
+    doc.moveDown();
+    doc.text(signature, { align: 'right' });
+    
+    doc.end();
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,11 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const cors = require('cors');
+const router = require('./routes');
+
 const app = express();
-const port = 3000;
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(router);
+
+app.listen(8080, () => console.log('server running on port 8080'));
